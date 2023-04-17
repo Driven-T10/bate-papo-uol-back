e252026e518c20a18f83a7c97e6945c0148313ff
leticia-chijo <leticia.chijo@gmail.com>
2023-04-17 20:23:29 -0300

    Requisitos: post e get de participantes e post de mensages

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import cors from "cors"
+import dayjs from "dayjs"
 import dotenv from "dotenv"
 import express from "express"
+import joi from "joi"
 import { MongoClient } from "mongodb"
 
 const app = express()
@@ -13,23 +15,86 @@ dotenv.config()
 // Conexão DB
 const mongoClient = new MongoClient(process.env.DATABASE_URL)
 try {
-	await mongoClient.connect()
-	console.log('MongoDB conectado!')
+    await mongoClient.connect()
+    console.log('MongoDB conectado!')
 } catch (err) {
-  console.log(err.message)
+    console.log(err.message)
 }
 
 const db = mongoClient.db()
 
 // Schemas
-app.get("/teste", (req, res) => {
-    res.send("Oiee!")
+const participantSchema = joi.object({ name: joi.string().required() })
+const messageSchema = joi.object({
+    from: joi.string().required(),
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().required().valid("message", "private_message")
 })
 
 
 // Endpoints
+app.post("/participants", async (req, res) => {
+    const { name } = req.body
 
+    const validation = participantSchema.validate(req.body, { abortEarly: false })
+    if (validation.error) {
+        return res.status(422).send(validation.error.details.map(detail => detail.message))
+    }
 
+    try {
+        const participant = await db.collection("participants").findOne({ name })
+        if (participant) return res.sendStatus(409)
+
+        const timestamp = Date.now()
+        await db.collection("participants").insertOne({ name, lastStatus: timestamp })
+
+        const message = {
+            from: name,
+            to: 'Todos',
+            text: 'entra na sala...',
+            type: 'status',
+            time: dayjs(timestamp).format("HH:mm:ss")
+        }
+
+        await db.collection("messages").insertOne(message)
+
+        res.sendStatus(201)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+})
+
+app.get("/participants", async (req, res) => {
+    try {
+        const participants = await db.collection("participants").find().toArray()
+        res.send(participants)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+})
+
+app.post("/messages", async (req, res) => {
+    const { user } = req.headers
+
+    const validation = messageSchema.validate({ ...req.body, from: user }, { abortEarly: false })
+    if (validation.error) {
+        return res.status(422).send(validation.error.details.map(detail => detail.message))
+    }
+
+    try {
+        const participant = await db.collection("participants").findOne({ name: user })
+        if (!participant) return res.sendStatus(422)
+
+        const message = { ...req.body, from: user, time: dayjs().format("HH:mm:ss") }
+        await db.collection("messages").insertOne(message)
+        res.sendStatus(201)
+
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+
+})
 
 
 // Deixa o app escutando, à espera de requisições
